1bd3db3187dd35e0d2a6d5edc24954028b759e94
root <root@pop-os.localdomain>
2020-01-16 16:34:32 -0300

    Updates

---
+++ precache-manifest.338a53057200b25fc6fd09a4b2c4184f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a81a3848e9b7d41b14b19c2c8fd397",
+    "url": "/reactjs-rocketshoes/index.html"
+  },
+  {
+    "revision": "8354d39974a2e8bb63e0",
+    "url": "/reactjs-rocketshoes/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8354d39974a2e8bb63e0",
+    "url": "/reactjs-rocketshoes/static/js/2.d88d76b8.chunk.js"
+  },
+  {
+    "revision": "68aed49bcdcbc067487a3d461801ff7e",
+    "url": "/reactjs-rocketshoes/static/js/2.d88d76b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b75181f0bb3a6f147b3c",
+    "url": "/reactjs-rocketshoes/static/js/main.20507b95.chunk.js"
+  },
+  {
+    "revision": "175e4227eba8b3a4d19c",
+    "url": "/reactjs-rocketshoes/static/js/runtime-main.41a2a479.js"
+  },
+  {
+    "revision": "38a4d24be6dc2aa32f0d0e454fc3cf35",
+    "url": "/reactjs-rocketshoes/static/media/Logo.38a4d24b.svg"
+  },
+  {
+    "revision": "38ea808a8078ca62133217454397665b",
+    "url": "/reactjs-rocketshoes/static/media/background.38ea808a.svg"
+  }
+]);
