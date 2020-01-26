a20a2fb530879760dd71ee52e8388790eb51df66
John Salgado <50679360+JohnSalgado@users.noreply.github.com>
2020-01-26 16:27:57 -0300

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API || 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API || 'https://my-json-server.typicode.com/JohnSalgado/reactjs-rocketshoes',
 })
 
 export default api
